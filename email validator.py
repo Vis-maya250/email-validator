52e729fb0486443e76dbd0fce2d67e4bfc900294
ajer23 <vismayavinod250@gmail.com>
2024-08-30 21:40:15 +0530

    email validator.py

--- email validator.py
+++ email validator.py
@@ -53,9 +53,9 @@ def main():
     
     // Print the result
     if is_valid:
-        print("**valid**")
+        print("**valid**)
     else:
-        print("**invalid")
+        print("**invalid**)
 
 // Run the main function if the script is executed directly
 if __name__ == "__main__":
